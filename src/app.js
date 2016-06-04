801387b937ddfab215722cad7ec07d30f6556c02
Yu Jiang <alex.yu.j@outlook.com>
2016-06-05 01:14:22 +0800

    added cookie keys although we are not really using

--- src/app.js
+++ src/app.js
@@ -9,16 +9,24 @@ import { serverLog as log } from './logger'
 const app = new Koa()
 
 /**
+ * Cookie keys
+ */
+app.keys = [ 'keys_should_be_hidden', 'but_im_just_a_demo' ]
+
+/**
+ * Middlewares
+ */
+app.use(serve(`${__dirname}/../public/dist`, { maxage: 1000 * 3600 * 24 * 365 }))
+app.use(views(`${__dirname}/../public/templates`, { extension: 'pug' }))
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+/**
  * Capture and log errors
  */
 app.on('error', err => {
   log.error({ err: err }, 'internal server error')
 })
-
-app.use(serve(`${__dirname}/../public/dist`, { maxage: 1000 * 3600 * 24 * 365 }))
-app.use(views(`${__dirname}/../public/templates`, { extension: 'pug' }))
-app.use(router.routes())
-app.use(router.allowedMethods())
 
 app.listen(port)
 
